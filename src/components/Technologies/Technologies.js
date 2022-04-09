da805d34152e8b641833b59b4dfd46e6902bbddc
Hamza-ben-yaflah <benyaflah0hamza@gmail.com>
2022-04-09 12:21:40 +0100

    Technologie Section

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import { DiFirebase, DiReact, DiZend } from 'react-icons/di';
-import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles/GlobalComponents';
-import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
+import React from "react";
+import { DiFirebase, DiReact } from "react-icons/di";
+import { AiOutlineAntDesign } from "react-icons/ai";
 
-const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+import {
+  Section,
+  SectionDivider,
+  SectionText,
+  SectionTitle,
+} from "../../styles/GlobalComponents";
+import {
+  List,
+  ListContainer,
+  ListItem,
+  ListParagraph,
+  ListTitle,
+} from "./TechnologiesStyles";
+
+const Technologies = () => (
+  <Section>
+    <SectionDivider />
+    <SectionTitle>Technologies</SectionTitle>
+    <SectionText>
+      I've worked with a range a technologies in the web development world from
+      Back-end to Design
+    </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size="3rem" />
+        <ListContainer>
+          <ListTitle>Back-End/CMS</ListTitle>
+          <ListParagraph>
+            Experiences with <br />
+            Contentful
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <DiReact size="3rem" />
+        <ListContainer>
+          <ListTitle>Front-End</ListTitle>
+          <ListParagraph>
+            Experiences with <br />
+            Html , Css , JavaScript , ReactJs
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+      <ListItem>
+        <AiOutlineAntDesign size="3rem" />
+        <ListContainer>
+          <ListTitle>UI/UX</ListTitle>
+          <ListParagraph>
+            Experiences with <br />
+            AntDesign
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
+  </Section>
 );
 
 export default Technologies;
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -4,7 +4,7 @@ export const projects = [
     description:
       "Using React, Node.js, Express & MongoDB you'll learn how to build a Full Stack MERN Application - from start to finish. The App is called Memories and it is a simple social media app that allows users to post interesting events that happened in their lives.",
     image: "/images/1.png",
-    tags: ["Nextjs", "Contentfull", "React", "Javascript"],
+    tags: ["Nextjs", "Contentful", "React", "Javascript"],
     visit: "https://football-shirt.vercel.app/",
     source: "https://github.com/Hamza-ben-yaflah/FootballShirt",
     id: 0,
